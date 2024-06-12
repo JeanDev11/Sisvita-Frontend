27dc42295683068bbf10e8c65ed9cdf18456b19f
Amy <amy.huaman2001@gmail.com>
2024-06-12 12:50:55 -0500

    Test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RegistrarTestComponent } from './registrar-test/registrar-test.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RegistrarTestComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/registrar-test/registrar-test.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registrar-test',
+  standalone: true,
+  imports: [],
+  templateUrl: './registrar-test.component.html',
+  styleUrl: './registrar-test.component.css'
+})
+export class RegistrarTestComponent {
+
+}
