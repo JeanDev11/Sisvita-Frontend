0b1c60ccfb414a5d15d7b92f9fdf150c825d96b5
Amy <amy.huaman2001@gmail.com>
2024-06-12 15:32:42 -0500

    Test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { RegistrarTestComponent } from './registrar-test/registrar-test.component';
+import { TestAnsiedadComponent } from './test-ansiedad/test-ansiedad.component';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RegistrarTestComponent],
+  imports: [RouterOutlet, TestAnsiedadComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { RegistrarTestComponent } from './registrar-test.component';
+import { HomeComponent } from './home.component';
 
-describe('RegistrarTestComponent', () => {
-  let component: RegistrarTestComponent;
-  let fixture: ComponentFixture<RegistrarTestComponent>;
+describe('HomeComponent', () => {
+  let component: HomeComponent;
+  let fixture: ComponentFixture<HomeComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RegistrarTestComponent]
+      imports: [HomeComponent]
     })
     .compileComponents();
     
-    fixture = TestBed.createComponent(RegistrarTestComponent);
+    fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
