b6596037e133d25544866a9ca8ead41250c2cb68
Amy <amy.huaman2001@gmail.com>
2024-06-26 22:25:20 -0500

    Comillas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MainComponent } from './components/main/main.component';
 import { LoginComponent } from './components/login/login.component';
+import { MapaComponent } from './components/mapa/mapa.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MainComponent, LoginComponent],
+  imports: [RouterOutlet, MainComponent, LoginComponent, MapaComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'] // Asegúrate de que esto sea 'styleUrls' en lugar de 'styleUrl'
 })
